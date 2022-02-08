74b67528c0ae5f8a0807e10df74266e2b2cc66cd
Nallasamy-Muthupavithra <90901613+Nallasamy-Muthupavithra@users.noreply.github.com>
2022-02-08 20:39:28 +0530

    Update Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -14,20 +14,27 @@ public class Main {
     return Float.compare(value, upperLimit) < 0;
   }
 
-  static boolean batteryIsOk(final float temperature, final float soc, final float chargeRate) {
-    if (!isInRange(temperature, 0, 45)) {
-      System.out.println("Temperature is out of range!");
-      return false;
-    }
-    else if (!isInRange(soc, 20, 80)) {
-      System.out.println("State of Charge is out of range!");
-      return false;
-    }
-    else if (!checkIfValueIsGreater(chargeRate, 0.8f)) {
-      System.out.println("Charge Rate is out of range!");
+
+  static boolean checkIfBatteryConditionsOk(final BatteryCondition batteryCondition, final float temperature,
+      final float lowerLimit, final float upperLimit) {
+    if (!isInRange(temperature, lowerLimit, upperLimit)) {
+      System.out.println(batteryCondition + " is out of range!");
       return false;
     }
     return true;
+  }
+
+  static boolean checkIfBatteryConditionsOk(final ChargeRate chargeRate, final float soc, final float upperLimit) {
+    if (!checkIfValueIsLesser(soc, upperLimit)) {
+      System.out.println(chargeRate + " is out of range!");
+      return false;
+    }
+    return true;
+  }
+
+  static boolean batteryIsOk(final float temperature, final float soc, final float chargeRate) {
+    return new Temperature().conditionIsOk(temperature, 0, 45) && new StateOfCharge().conditionIsOk(soc, 20, 80) &&
+        new ChargeRate().conditionIsOk(chargeRate, Float.NaN, 0.8f);
   }
 
   static void testIsValueLesser() {
@@ -35,9 +42,6 @@ public class Main {
     assert (checkIfValueIsLesser(8, 5) == false);
   }
 
-  /**
-   *
-   */
   private static void testIsValueGreater() {
     assert (checkIfValueIsGreater(8, 5) == true);
     assert (checkIfValueIsGreater(3, 5) == false);
@@ -50,13 +54,6 @@ public class Main {
     assert (isInRange(7, 4, 6) == false); // range 4-6
   }
 
-  public static void main(final String[] args) {
-    testIsValueGreater();
-    testIsValueLesser();
-    testIsInRange();
-    testBatteryIsOk();
-  }
-
   static void testBatteryIsOk() {
     assert (batteryIsOk(25, 70, 0.7f) == true); // temperature,soc, chargeRate in range
     assert (batteryIsOk(50, 85, 0.9f) == false); // temperature,soc, chargeRate - out of range
@@ -65,5 +62,44 @@ public class Main {
     assert (batteryIsOk(40, 90, 0.7f) == false); // soc - out of range(high)
     assert (batteryIsOk(40, 20, 0.7f) == false); // soc - out of range(low)
     assert (batteryIsOk(40, 70, 1.0f) == false); // charge rate - out of range (high)
+  }
+
+  public static void main(final String[] args) {
+    testIsValueGreater();
+    testIsValueLesser();
+    testIsInRange();
+    testBatteryIsOk();
+  }
+}
+
+abstract class BatteryCondition {
+
+  String condition;
+
+  abstract boolean conditionIsOk(float value, float upperlimit, float lowerlimit);
+}
+
+class Temperature extends BatteryCondition {
+
+
+  @Override
+  public boolean conditionIsOk(final float temp, final float lowerLimit, final float upperLimit) {
+    return Main.checkIfBatteryConditionsOk(this, temp, lowerLimit, upperLimit);
+  }
+}
+
+class StateOfCharge extends BatteryCondition {
+
+  @Override
+  public boolean conditionIsOk(final float soc, final float lowerLimit, final float upperLimit) {
+    return Main.checkIfBatteryConditionsOk(this, soc, lowerLimit, upperLimit);
+  }
+}
+
+class ChargeRate extends BatteryCondition {
+
+  @Override
+  public boolean conditionIsOk(final float soc, final float lowerLimit, final float upperLimit) {
+    return Main.checkIfBatteryConditionsOk(this, soc, upperLimit);
   }
 }
