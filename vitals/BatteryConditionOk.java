584e9bb158fcd0f5e1a5a4ba40ad52117a4f4606
Nallasamy-Muthupavithra <90901613+Nallasamy-Muthupavithra@users.noreply.github.com>
2022-02-16 14:43:59 +0530

    Update BatteryConditionOk.java

--- vitals/BatteryConditionOk.java
+++ vitals/BatteryConditionOk.java
@@ -59,19 +59,63 @@ public class BatteryConditionOk {
     return true;
   }
 
-  static boolean batteryIsOk(final float temperature, final float soc, final float chargeRate) {
-    Temperature tempObject = (final float value, final float lowerLimit,
-        final float upperLimit) -> checkIfBatteryConditionOk("Temperature", value, lowerLimit, upperLimit, true);
+  static boolean batteryIsOk(final String temperature, final String soc, final String chargeRate) {
 
-    StateOfCharge stateOfChargeObject = (final float value, final float lowerLimit,
-        final float upperLimit) -> checkIfBatteryConditionOk("State Of Charge", value, lowerLimit, upperLimit, true);
+    Temperature tempObject = (value, lowerLimit, upperLimit) -> checkIfBatteryConditionOk("Temperature", value,
+        lowerLimit, upperLimit, true);
 
-    ChargeRate chargeRateObject =
-        (final float value, final float upperLimit) -> checkIfChargeRateOk("Charge Rate", value, upperLimit, true);
-    return tempObject.conditionIsOk(temperature, 0, 45) && stateOfChargeObject.conditionIsOk(soc, 20, 80) &&
-        chargeRateObject.conditionIsOk(chargeRate, 0.8f);
+    StateOfCharge stateOfChargeObject = (value, lowerLimit, upperLimit) -> checkIfBatteryConditionOk("State Of Charge",
+        value, lowerLimit, upperLimit, true);
+
+    ChargeRate chargeRateObject = (value, upperLimit) -> checkIfChargeRateOk("Charge Rate", value, upperLimit, true);
+
+    float tempInCelcius = getTemperatureInCelcius(temperature);
+    float socInPercent = getSocInPercent(soc);
+    float chargeRateInCRate = getChargeRateInCRate(chargeRate);
+
+    return tempObject.conditionIsOk(tempInCelcius, 0, 45) && stateOfChargeObject.conditionIsOk(socInPercent, 20, 80) &&
+        chargeRateObject.conditionIsOk(chargeRateInCRate, 0.8f);
   }
 
+  private static float getChargeRateInCRate(final String chargeRate) {
+    // ChargeRate Eg: 0.8 C
+    float chargeRateInCRate = 0;
+    String[] unitAndValue = extractUnitAndValue(chargeRate);
+    float value = Float.parseFloat(unitAndValue[0]);
+    String unit = unitAndValue[1];
+    if (unit.equals("C")) {
+      chargeRateInCRate = value;
+    }
+    return chargeRateInCRate;
+  }
+
+  private static float getSocInPercent(final String soc) {
+    // SOC Eg: 40 %
+    float socInPercent = 0;
+    String[] unitAndValue = extractUnitAndValue(soc);
+    float value = Float.parseFloat(unitAndValue[0]);
+    String unit = unitAndValue[1];
+    if (unit.equals("%")) {
+      socInPercent = value;
+    }
+    return socInPercent;
+  }
+
+  private static float getTemperatureInCelcius(final String temperature) {
+    // Eg: Farenheit - 100 F, Celcius - 40 C
+    String[] unitAndValue = extractUnitAndValue(temperature);
+    float value = Float.parseFloat(unitAndValue[0]);
+    String unit = unitAndValue[1];
+    if (unit.equals("F")) {
+      value = ((value - 32) * 5) / 9;
+    }
+    return value;
+  }
+
+  private static String[] extractUnitAndValue(final String valueInStringFormat) {
+    String regex = "\\s";
+    return valueInStringFormat.split(regex);
+  }
 
   interface Temperature {
 
